cb0e98af843eb3527ed6d6dce9a1add6dfb0a039
Detlef von der Thuesen <dthuesen@gmx.de>
2016-11-09 17:53:37 +0100

    Spinner with *ngIf and iteration with *ngFor

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from './post.service';
-import{ IPost } from './post'
+import { IPost } from './post';
 
 @Component({
     selector: 'posts',
     template: `
       <h1>Posts</h1>
+      <div *ngIf="isLoading">
+        <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+      </div>
       <p>from the posts.component.ts</p>
-      <div><code>{{response}}</code></div>
+      <div *ngFor="let post of posts">
+        <h3>{{post.title}}</h3>
+        <p>{{post.body}}</p>
+        <hr />
+      </div>
+      
       <button (click)="this.getPosts()">Get Posts</button>
     `,
 })
@@ -15,17 +23,20 @@ export class PostsComponent implements OnInit {
 
   errorMessage: string;
   posts: IPost[];
+  isLoading: boolean = true;
 
   constructor(private _postService: PostService) {
   }
 
   ngOnInit(): void {
-
+    this.isLoading = false;
   }
 
   getPosts(): void {
         this._postService.getPosts()
-                .subscribe(posts => this.posts = posts,
+                .subscribe(posts => {
+                                      this.posts = posts;
+                                    },
                            error => this.errorMessage = <any>error);
     }
 }
